03c1ded424af64a250efd1335feb7dba32f001db
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 09:49:34 -0300

    Finish second exercise
    Did the repeat string exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string_repeat, number_of_repeats) {
+    if (number_of_repeats < 0) return "ERROR";
 
+    let final_string = '';
+
+    for (let i=0; i < number_of_repeats; i++) {
+        final_string += string_repeat;
+    }
+
+    return final_string;
 };
 
 // Do not edit below this line
