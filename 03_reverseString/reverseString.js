96faf1ba9aa58a3854f4f0a9aae2e1c354f75845
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 10:18:10 -0300

    Finish third exercise
    This was a nice one. I managed to make a clean code just by checking the
    hints in the README file and using node's auto complete feature to check
    the results when working with created arrays. My experience with python
    surely helped a lot here too.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(original_string) {
+    const char_list = original_string.split('');
+    char_list.reverse()
+    const reversed_string = char_list.join('');
 
+    return reversed_string;
 };
 
 // Do not edit below this line
