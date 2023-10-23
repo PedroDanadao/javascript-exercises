0ef70fee58beb4cf39526e72f3580eab7f45af81
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 18:26:01 -0300

    Finish the tenth exercise
    This time with a classic fibonacci position.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(numberPosition) {
+    // convert any string numbers to a number
+    numberPosition = Number(numberPosition);
+    
+    if (numberPosition < 0) return "OOPS";
 
+    let previousNumber = 1;
+    let beforePrevious = 0;
+    let currentNumber = 1;
+
+    for (let i=1; i < numberPosition; i++) {
+        currentNumber = previousNumber + beforePrevious;
+
+        beforePrevious = previousNumber;
+        previousNumber = currentNumber;
+    }
+
+    return currentNumber;
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
