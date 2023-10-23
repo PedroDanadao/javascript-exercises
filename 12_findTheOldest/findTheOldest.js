c71711b959adce3ff0a8614b3895ba1f4eee96d0
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 19:20:22 -0300

    Finish the final javascript exercise
    That's it. Did all the exercises from part 4 of the javascript
    fundamentals. It was a nice one.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
 
+    for (person of people) {
+        if (getYearsLived(person) > getYearsLived(oldestPerson)) {
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
+
+function getYearsLived (person) {
+    const currentYear = Number(new Date().getFullYear());
+
+    lastLivedYear = "yearOfDeath" in person ? person.yearOfDeath : currentYear;
+
+    return lastLivedYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
