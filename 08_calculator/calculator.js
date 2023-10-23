614c423ad0a324830211f0d94f794c1bae609ca9
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 16:57:08 -0300

    Finish the eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+    let result = 0;
+
+    for (i of numArray) {
+        result += i;
+    }
+
+    return result
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+    let result = 1;
 
+    for (i of numArray) {
+        result *= i;
+    }
+
+    return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    let result = 1;
+
+    for (i=0; i < pow; i++) {
+        result *= num;
+    }
+
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    result = 1;
+
+    for (i=num; i > 1; i--) {
+        result *= i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
