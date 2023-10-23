ef36086f669e4c4344bb9c0564342f6c8cf70365
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-10-23 17:55:28 -0300

    Finish the ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (checkString) {
+    let safeString = removePunctuation(checkString);
+    safeString = safeString.toLowerCase()
 
+    // these two variables will be used to facilitate code reading
+    const ssLength = safeString.length;
+    const ssMaxIndex = ssLength - 1
+
+    const midPosition = Math.floor( (ssLength - 1) / 2 );
+
+    for (let i=0; i < midPosition; i++) {
+        let charI = safeString[i];
+        let otherEndChar = safeString[ssMaxIndex - i];
+
+        if (charI !== otherEndChar) return false;
+    }
+
+    return true;
 };
+
+function removePunctuation(passedString) {
+    return passedString.replace(/[\.,?!: ]/g, "");
+}
+
+palindromes("raccar!");
 
 // Do not edit below this line
 module.exports = palindromes;
